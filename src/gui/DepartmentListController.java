6d3c98f52dd552ac01a1fe58bb347da60c3e5fa4
michelPech <michelhphz@hotmail.com>
2020-06-24 21:42:01 -0300

    Salvando um novo departamento

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -84,6 +84,7 @@ public class DepartmentListController implements Initializable {
 			
 			DepartmentFormController controller = loader.getController();
 			controller.setDepartment(obj);
+			controller.setService(new DepartmentService());
 			controller.updateFormData();
 			
 			Stage dialogStage = new Stage();
